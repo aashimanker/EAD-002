2620b0522b54730812a71ff3d336f049650cd906
Aashi Manker <aashimanker3007@gmail.com>
2024-11-09 16:00:55 +0530

    Added Timer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
 import Register from './Pages/Register'
 import Password from './Pages/Password'
+import Timer from './Pages/Timer'
 
 function App() {
 
   return (
     <>
     {/* <Register/> */}
-    <Password/>
+    {/* <Password/> */}
+    <Timer/>
     </>
   )
 }
